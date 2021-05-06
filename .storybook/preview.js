9f8e3f7ad78c900d4c273d21fc59164233185d37
Ianka Karuzia <iankakaruzia@gmail.com>
2021-05-05 22:12:59 -0300

    feat: :sparkles: setting up storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,16 @@
+import { addDecorator } from '@storybook/react'
+import { ThemeProvider } from 'styled-components'
+import { withNextRouter } from 'storybook-addon-next-router'
+import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
+
+addDecorator(withNextRouter())
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Story />
+    </ThemeProvider>
+  )
+]
