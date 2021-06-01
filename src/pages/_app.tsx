04258fea5613306a69c59f066f0672e2df1acfc5
Ianka Karuzia <iankakaruzia@gmail.com>
2021-06-01 19:40:26 -0300

    feat: :sparkles: add progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 import { ThemeProvider } from 'styled-components'
+import NextProgress from 'nextjs-progressbar'
 
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
@@ -19,6 +20,12 @@ function App({ Component, pageProps }: AppProps) {
         />
       </Head>
       <GlobalStyles />
+      <NextProgress
+        color='#2F80ED'
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={5}
+      />
       <Component {...pageProps} />
     </ThemeProvider>
   )
