8a6e1214b89d8752b81e12dbd17c16a6b9e92e01
Ianka Karuzia <iankakaruzia@gmail.com>
2021-07-27 22:14:42 -0300

    chore: :arrow_up: upgrade storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,8 @@
 import '../.jest/next-image.mock'
-import { addDecorator } from '@storybook/react'
 import { ThemeProvider } from 'styled-components'
-import { withNextRouter } from 'storybook-addon-next-router'
+import { RouterContext } from 'next/dist/next-server/lib/router-context'
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
-
-addDecorator(withNextRouter())
 
 export const decorators = [
   (Story) => (
@@ -15,3 +12,9 @@ export const decorators = [
     </ThemeProvider>
   )
 ]
+
+export const parameters = {
+  nextRouter: {
+    Provider: RouterContext.Provider,
+  },
+}
