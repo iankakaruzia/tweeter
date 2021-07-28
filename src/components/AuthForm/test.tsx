510cb0813a6b55ec0a0e122af013b9f9a818da1f
Ianka Karuzia <iankakaruzia@gmail.com>
2021-07-27 22:26:59 -0300

    chore: :arrow_up: update libs

--- src/components/AuthForm/test.tsx
+++ src/components/AuthForm/test.tsx
@@ -13,64 +13,56 @@ describe('<AuthForm />', () => {
     )
 
     expect(container.parentElement).toMatchInlineSnapshot(`
-      .c0 form {
-        display: -webkit-box;
-        display: -webkit-flex;
-        display: -ms-flexbox;
-        display: flex;
-        -webkit-flex-direction: column;
-        -ms-flex-direction: column;
-        flex-direction: column;
-      }
+.c0 form {
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-flex-direction: column;
+  -ms-flex-direction: column;
+  flex-direction: column;
+}
 
-      .c0 form .sc-jSFjdj + .sc-jSFjdj {
-        margin-top: 1.4rem;
-      }
+.c1 {
+  font-size: 1.4rem;
+  line-height: 1.9rem;
+  -webkit-letter-spacing: -0.035em;
+  -moz-letter-spacing: -0.035em;
+  -ms-letter-spacing: -0.035em;
+  letter-spacing: -0.035em;
+  color: #828282;
+  -webkit-align-self: center;
+  -ms-flex-item-align: center;
+  align-self: center;
+  margin-top: 3rem;
+}
 
-      .c0 form .sc-gKAaRy {
-        margin-top: 2.2rem;
-      }
+.c1 a {
+  color: #8db9f5;
+}
 
-      .c1 {
-        font-size: 1.4rem;
-        line-height: 1.9rem;
-        -webkit-letter-spacing: -0.035em;
-        -moz-letter-spacing: -0.035em;
-        -ms-letter-spacing: -0.035em;
-        letter-spacing: -0.035em;
-        color: #828282;
-        -webkit-align-self: center;
-        -ms-flex-item-align: center;
-        align-self: center;
-        margin-top: 3rem;
-      }
+.c1 a:hover {
+  color: #a4c8f7;
+}
 
-      .c1 a {
-        color: #8db9f5;
-      }
-
-      .c1 a:hover {
-        color: #a4c8f7;
-      }
-
-      <body>
-        <div>
-          <div
-            class="c0"
-          >
-            <div
-              class="c1"
-            >
-              Some random 
-              <a
-                href="#"
-              >
-                link
-              </a>
-            </div>
-          </div>
-        </div>
-      </body>
-    `)
+<body>
+  <div>
+    <div
+      class="c0"
+    >
+      <div
+        class="c1"
+      >
+        Some random 
+        <a
+          href="#"
+        >
+          link
+        </a>
+      </div>
+    </div>
+  </div>
+</body>
+`)
   })
 })
