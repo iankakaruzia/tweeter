bd7c284bb871485a6f0058c8f09271d0b6938f59
Ianka Karuzia <iankakaruzia@gmail.com>
2022-08-04 16:37:38 -0300

    chore: update libs

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const withPWA = require('next-pwa')
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withPWA({
+  compiler: {
+    styledComponents: true
+  },
   pwa: {
     dest: 'public',
     disable: !isProd
